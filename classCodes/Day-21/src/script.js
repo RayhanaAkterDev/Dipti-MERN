54359ef46bae9cdc61e6c8708c61a50cf086d43e
Rayhana Akter Sumaya <rayhanaaktersumaya.dev@gmail.com>
2026-08-19 13:03:45 +0600

    feat(classCodes): added Day-21 class codes

---
+++ classCodes/Day-21/src/script.js
@@ -0,0 +1,18 @@
+const fruites = ["mango", 'apple', 'banana'];
+
+// array destructuring
+const [frt1, frt2] = fruites;
+console.log(frt1);
+console.log(frt2);
+
+const info = {
+    name: 'sumaya',
+    age: 25,
+}
+
+// object destructuring
+const { name, age } = info;
+console.log(name);
+console.log(age);
+
+console.log(info.name);
